ae4f6d3dc061be5c131622549ed1fe3f5d3b6a46
Viktor Georgiev <v.georgiev90@abv.bg>
2019-02-26 12:08:19 +0200

    Update master_add.rb

--- cookbooks/k8s/recipes/master_add.rb
+++ cookbooks/k8s/recipes/master_add.rb
@@ -3,11 +3,11 @@ bash 'get_certs' do
   environment 'IP' => data_bag_item('k8s', 'info')['primarymaster']
   code <<-EOH
     mkdir -p /etc/kubernetes/pki /etc/kubernetes/pki/etcd /root/.kube
-    scp -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/ca.* /etc/kubernetes/pki/.
-    scp -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/sa.* /etc/kubernetes/pki/.
-    scp -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/front-proxy-ca.* /etc/kubernetes/pki/.
-    scp -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/etcd/ca.* /etc/kubernetes/pki/etcd/.
-    scp -i /root/.ssh/k8s root@$IP:/etc/kubernetes/admin.conf /root/.kube/config
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/ca.* /etc/kubernetes/pki/.
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/sa.* /etc/kubernetes/pki/.
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/front-proxy-ca.* /etc/kubernetes/pki/.
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/k8s root@$IP:/etc/kubernetes/pki/etcd/ca.* /etc/kubernetes/pki/etcd/.
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/k8s root@$IP:/etc/kubernetes/admin.conf /root/.kube/config
     EOH
   not_if { ::File.exists?("/etc/kubernetes/manifests/kube-apiserver.yaml") }
 end
