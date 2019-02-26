32592e9cbdae5ef85b683973bac5bd3bdc04d25f
Viktor Georgiev <v.georgiev90@abv.bg>
2019-02-26 12:00:39 +0200

    Update master_init.rb

--- cookbooks/k8s/recipes/master_init.rb
+++ cookbooks/k8s/recipes/master_init.rb
@@ -3,23 +3,16 @@ template '/root/cluster-init.yml' do
   variables(
     endpoint: data_bag_item('k8s', 'info')['endpoint'],   
     podsubnet: data_bag_item('k8s', 'info')['podsubnet'], 
-    apisans: data_bag_item('k8s', 'info')['apisans']     
+    apisans: data_bag_item('k8s', 'info')['apisans'],
+    token: data_bag_item('k8s', 'info')['token']
   )
 end
 
-
 bash 'master_init' do
-  environment 'TOKEN' => data_bag_item('k8s', 'info')['token']
   code <<-EOH
-    kubeadm init --config=/root/cluster-init.yml --token=$TOKEN --ignore-preflight-errors=all
+    kubeadm init --config=/root/cluster-init.yml --ignore-preflight-errors=all
     EOH
   not_if { ::File.exists?("/etc/kubernetes/manifests/kube-apiserver.yaml") }
-end
-
-bash 'print_join_command' do
-  code <<-EOH
-    kubeadm token create --print-join-command
-    EOH
 end
 
 bash 'admin_config' do
