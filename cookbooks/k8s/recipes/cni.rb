ad0f509306bc0ef5f4d3ab0c7f5edabc3211cb9f
Viktor Georgiev <v.georgiev90@abv.bg>
2019-02-26 13:38:15 +0200

    Update cni.rb

--- cookbooks/k8s/recipes/cni.rb
+++ cookbooks/k8s/recipes/cni.rb
@@ -3,4 +3,5 @@ bash 'pod_network' do
   code <<-EOH
     kubectl --kubeconfig=/root/.kube/config apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=$RANGE"
     EOH
+  not_if "kubectl --kubeconfig=/root/.kube/config get pods --all-namespaces | grep weave 2>/dev/null 1>&2"
 end
